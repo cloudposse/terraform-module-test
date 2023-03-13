b525db4d84d9172252a2b59c398ec6a27afef044
Max Lobur <max_lobur@outlook.com>
2023-03-13 23:22:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "random_integer" "example" {
   count   = module.this.enabled ? 1 : 0
+  testing = local.testing ? 0 : 1
   min     = 1
   max     = 50000
   keepers = {
@@ -8,5 +9,6 @@ resource "random_integer" "example" {
 }
 
 locals {
+  testing = true
   example = format("%v %v", var.example, join("", random_integer.example[*].result))
 }
