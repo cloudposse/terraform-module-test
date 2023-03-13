521ddc213b4bf8c0a8e51e81a42609525e2d9495
Max Lobur <max_lobur@outlook.com>
2023-03-13 22:25:17 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 resource "random_integer" "example" {
   count = module.this.enabled ? 1 : 0
-  test_linting = 0
 
   min = 1
   max = 50000
@@ -8,6 +7,8 @@ resource "random_integer" "example" {
   example = var.example
   }
 }
+
+resource "test_linting" "test" {}
 
 locals {
   example = format("%v %v", var.example, join("", random_integer.example[*].result))
