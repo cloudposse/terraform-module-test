a3dab87141022923819de2fe5357daf85d34aa8a
Max Lobur <max_lobur@outlook.com>
2023-03-16 21:37:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "random_integer" "example" {
   count   = module.this.enabled ? 1 : 0
   testing = local.testing ? 1 : 0
   min     = 1
+    
   max     = 50000
   keepers = {
     example = var.example
