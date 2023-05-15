449bde8abf314c1bda8221860a1129fe1a98f5cb
Max Lobur <max_lobur@outlook.com>
2023-05-15 20:53:23 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,12 @@ output "example" {
   value       = module.this.enabled ? local.example : null
 }
 
+output "test" {
+  description = "Example output"
+  value       = module.this.enabled ? local.none : null
+}
+
+
 output "random" {
   description = "Stable random number for this example"
   value       = module.this.enabled ? join("", random_integer.example[*].result) : null
