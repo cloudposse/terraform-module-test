dbc44fdde346f3e89d554f701250d35fce55587d
Igor Rodionov <goruha@gmail.com>
2024-05-01 00:41:51 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "random_integer" "example" {
-  count   = module.this.enabled ? 1 : 0
-  min     = 1
-  max     = 20000
+  count = module.this.enabled ? 1 : 0
+  min   = 1
+  max   = 20000
   keepers = {
     testing = local.testing ? 1 : 0
     example = var.example
